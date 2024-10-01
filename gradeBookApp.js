a6c1d71e4ed9cc75e4315a4137bfa0cb11e3da80
sethk3 <threegartz@gmail.com>
2024-09-30 20:14:37 -0600

    first two gradebook func

---
+++ gradeBookApp.js
@@ -0,0 +1,29 @@
+function getAverage(scores) {
+    const total = scores.reduce((sum, num) => sum + num, 0)
+  
+    return total / scores.length
+  }
+  
+  console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+  console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+
+function getGrade(score) {
+    if (score === 100) {
+      return "A++"
+    } else if (score >= 90 && score <= 99) {
+      return "A"
+    } else if (score >= 80 && score <= 89) {
+      return "B"
+    } else if (score >= 70 && score <= 79) {
+      return "C"
+    } else if (score >= 60 && score <= 69) {
+      return "D"
+    } else {
+      return "F"
+    }
+  }
+  
+  console.log(getGrade(96));
+  console.log(getGrade(82));
+  console.log(getGrade(56));
