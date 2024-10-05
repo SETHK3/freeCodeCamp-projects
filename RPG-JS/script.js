6ce730cc565b45be3444bb5bf8f0082d32e25e9b
sethk3 <threegartz@gmail.com>
2024-10-04 23:46:45 -0600

    adds missed attack probability

--- RPG-JS/script.js
+++ RPG-JS/script.js
@@ -183,7 +183,11 @@ function goCave() {
         text.innerText = "The " + monsters[fighting].name + " attacks.";
         text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
         health -= getMonsterAttackValue(monsters[fighting].level);
-        monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+        if (isMonsterHit()) {
+            monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+        } else {
+            text.innerText = " You miss."
+        }
         healthText.innerText = health;
         monsterHealthText.innerText = monsterHealth;
         if (health <= 0) {
@@ -201,6 +205,10 @@ function getMonsterAttackValue(level) {
     const hit = (level * 5) - (Math.floor(Math.random() * xp))
     console.log(hit)
     return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+    return Math.random() > .2 || health < 20;
 }
 
 function dodge() {
