69d6252d9327d1554f65e7931241986326caa819
sethk3 <threegartz@gmail.com>
2024-10-04 23:52:35 -0600

    adds weapon breaking probability

--- RPG-JS/script.js
+++ RPG-JS/script.js
@@ -199,6 +199,10 @@ function goCave() {
                 defeatMonster();
             }
   }
+  if (Math.random() <= .1 && inventory.length !== 1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeaponIndex--
+  }
 }
 
 function getMonsterAttackValue(level) {
