425ae451d39891bf151cf6e98d7510d2ddfd0b2c
sethk3 <threegartz@gmail.com>
2024-10-04 23:34:50 -0600

    adds dynamism to health in attack func

--- RPG-JS/script.js
+++ RPG-JS/script.js
@@ -182,7 +182,7 @@ function goCave() {
     function attack() {
         text.innerText = "The " + monsters[fighting].name + " attacks.";
         text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-        health -= monsters[fighting].level;
+        health -= getMonsterAttackValue(monsters[fighting].level);
         monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
         healthText.innerText = health;
         monsterHealthText.innerText = monsterHealth;
